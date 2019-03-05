b23e15a7bfeb50c8b8cfbc136310974924b91d74
Hussein Habibi Juybari <Hussein.Juybari@gmail.com>
2019-03-05 17:24:27 +0330

    Add play service dependency and

--- Assets/Scripts/ChabokPush.cs
+++ Assets/Scripts/ChabokPush.cs
@@ -20,18 +20,29 @@ public class ChabokPush : MonoBehaviour {
 
     private void Init()
     {
+        Log("~~~~~~> 0 INIIIIITTTT");
+
         using (AndroidJavaClass mainClass = new AndroidJavaClass("com.adpdigital.push.AdpPushClient"))
         {
-            Log(1);
+            Log("~~~~~~> 1 Get UnityPlayer class");
             AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
             AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+            Log("~~~~~~> 2 Get getApplicationContexts");
+
             AndroidJavaObject context = activity.Call<AndroidJavaObject>("getApplicationContext");
-            Log(2);
-            mainClass.CallStatic<AndroidJavaObject>("init", context, unityPlayer, "tojfojwo/418500099884", "71ccffab0597699fc338b8709bf6b32a18024871", "nacjerkogo", "rofuczapoz");
-            Log(3);
-            Log(mainClass.CallStatic<AndroidJavaObject>("get"));
-            Log(4);
-            Log(mainClass.CallStatic<AndroidJavaObject>("get").Call<AndroidJavaObject>("register", SystemInfo.deviceUniqueIdentifier));
+
+            Log("~~~~~~> 3 Geting init ");
+            mainClass.CallStatic<AndroidJavaObject>("init", context, unityPlayer, "adp-nms-push/845225163503", "e2100f0d7e071c7450f04e530bda746da2fc493b", "adp", "test");
+
+            Log("~~~~~~> 4 Geting setDevelopment ");
+            Log(mainClass.CallStatic<AndroidJavaObject>("get").Call<AndroidJavaObject>("setDevelopment", true));
+
+            mainClass.CallStatic<AndroidJavaObject>("get").Get<AndroidJavaClass>("foreground").Call("foreground",true);
+
+
+            Log("~~~~~~> 5 Geting register ");
+            Log(mainClass.CallStatic<AndroidJavaObject>("get").Call<AndroidJavaObject>("register", "98933195"));
+
             Log(5);
         }
     }
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -7,7 +7,6 @@ public class Main : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("###################33");
     }
 
     // Update is called once per frame
