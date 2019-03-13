c11d7114f3e9290e097f896d49eb1d4330f659cd
Hussein Habibi Juybari <Hussein.Juybari@gmail.com>
2019-03-13 16:26:20 +0330

    Add callback for assign tag to users

---
+++ Assets/Scripts/ChabokPush/AndroidPluginCallback.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Application
+{
+    public class AndroidPluginCallback : AndroidJavaProxy
+    {
+
+        public AndroidPluginCallback() : base("com.adpdigital.push.Callback") { }
+
+        public void onSuccess(AndroidJavaObject count)
+        {
+            Debug.Log("ENTER callback onSuccess: " + count);
+        }
+
+        public void onError(AndroidJavaObject exception)
+        {
+            Debug.Log("ENTER callback onError: " + exception);
+        }
+    }
+}
