a38bc5d984abf5eb1ff2ea8caf21909924fa5068
Hussein Habibi Juybari <Hussein.Juybari@gmail.com>
2019-03-13 16:31:00 +0330

    Add event to android callback class

--- Assets/Scripts/ChabokPush/AndroidPluginCallback.cs
+++ Assets/Scripts/ChabokPush/AndroidPluginCallback.cs
@@ -5,16 +5,29 @@ namespace Application
 {
     public class AndroidPluginCallback : AndroidJavaProxy
     {
+        public delegate void OnSuccessEvent(AndroidJavaObject count);
+        public delegate void onErrorEvent(AndroidJavaObject exception);
+
+        public event OnSuccessEvent OnSuccess;
+        public event onErrorEvent OnError;
 
         public AndroidPluginCallback() : base("com.adpdigital.push.Callback") { }
 
-        public void onSuccess(AndroidJavaObject count)
+        private void onSuccess(AndroidJavaObject count)
         {
+            if (OnSuccess != null)
+            {
+                OnSuccess.Invoke(count);
+            }
             Debug.Log("ENTER callback onSuccess: " + count);
         }
 
         public void onError(AndroidJavaObject exception)
         {
+            if (OnError != null)
+            {
+                OnError.Invoke(exception);
+            }
             Debug.Log("ENTER callback onError: " + exception);
         }
     }
