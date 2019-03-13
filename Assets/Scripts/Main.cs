ddab5bf8cd02326c26697db5d86e81f4418eee79
Hussein Habibi Juybari <Hussein.Juybari@gmail.com>
2019-03-13 16:32:29 +0330

    Create sample for ChabokPush bridge

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Application;
 using UnityEngine;
 
 public class Main : MonoBehaviour
@@ -7,6 +8,31 @@ public class Main : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        AndroidJavaObject unityPlayerActivity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject currentActivity = unityPlayerActivity.GetStatic<AndroidJavaObject>("currentActivity");
+        AndroidJavaObject context = currentActivity.Call<AndroidJavaObject>("getApplicationContext");
+
+        var chabok = new ChabokPush();
+        chabok.Init(context, unityPlayerActivity, "APP_ID/SENDER_ID", "API_KEY", "USERNAME", "PASSWORD");
+        chabok.SetDevelopment(true);
+
+        var userId = chabok.GetUserId();
+        if (userId != null)
+        {
+            chabok.Register(userId);
+        }
+        else
+        {
+            chabok.RegisterAsGuest();
+        }
+
+        var callback = new AndroidPluginCallback();
+        callback.OnSuccess += (count) => { };
+        callback.OnError += (exception) => { };
+
+        chabok.AddTag("Test", callback);
+
+        chabok.Track("LIKE");
     }
 
     // Update is called once per frame
