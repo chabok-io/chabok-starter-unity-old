52843fce6094114df9b2b7d60737304b9b58fa9e
Hussein Habibi Juybari <Hussein.Juybari@gmail.com>
2019-06-16 10:25:59 +0430

    Remove Application namespace in AndroidCallBack class

--- Assets/Scripts/ChabokPush/AndroidPluginCallback.cs
+++ Assets/Scripts/ChabokPush/AndroidPluginCallback.cs
@@ -1,34 +1,30 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
-namespace Application
+public class AndroidPluginCallback : AndroidJavaProxy
 {
-    public class AndroidPluginCallback : AndroidJavaProxy
+    public delegate void OnSuccessEvent(AndroidJavaObject count);
+    public delegate void onErrorEvent(AndroidJavaObject exception);
+
+    public event OnSuccessEvent OnSuccess;
+    public event onErrorEvent OnError;
+
+    public AndroidPluginCallback() : base("com.adpdigital.push.Callback") { }
+
+    private void onSuccess(AndroidJavaObject count)
     {
-        public delegate void OnSuccessEvent(AndroidJavaObject count);
-        public delegate void onErrorEvent(AndroidJavaObject exception);
-
-        public event OnSuccessEvent OnSuccess;
-        public event onErrorEvent OnError;
-
-        public AndroidPluginCallback() : base("com.adpdigital.push.Callback") { }
-
-        private void onSuccess(AndroidJavaObject count)
+        if (OnSuccess != null)
         {
-            if (OnSuccess != null)
-            {
-                OnSuccess.Invoke(count);
-            }
-            Debug.Log("ENTER callback onSuccess: " + count);
+            OnSuccess.Invoke(count);
         }
+        Debug.Log("ENTER callback onSuccess: " + count);
+    }
 
-        public void onError(AndroidJavaObject exception)
+    public void onError(AndroidJavaObject exception)
+    {
+        if (OnError != null)
         {
-            if (OnError != null)
-            {
-                OnError.Invoke(exception);
-            }
-            Debug.Log("ENTER callback onError: " + exception);
+            OnError.Invoke(exception);
         }
+        Debug.Log("ENTER callback onError: " + exception);
     }
 }
