7e9e65d0b8501bab4ea846676d3f5612b94cc43f
Hussein Habibi Juybari <Hussein.Juybari@gmail.com>
2019-09-07 23:51:02 +0430

    Add text to sence

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Main : MonoBehaviour
 {
+    public Text txt;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,13 +15,15 @@ public class Main : MonoBehaviour
         AndroidJavaObject currentActivity = unityPlayerActivity.GetStatic<AndroidJavaObject>("currentActivity");
         AndroidJavaObject context = currentActivity.Call<AndroidJavaObject>("getApplicationContext");
 
+        Log("Initialize chabok............... " + gameObject.name);
         var chabok = new ChabokPush();
+
         chabok.Init(context,
              unityPlayerActivity,
-              "APP_ID/SENDERID",
-               "API_KEY",
-                "USERNAME",
-                 "PASSWORD");
+              "chabok-starter/839879285435",
+               "70df4ae2e1fd03518ce3e3b21ee7ca7943577749",
+                "chabok-starter",
+                 "chabok-starter");
         chabok.SetDevelopment(true);
 
         //chabok.SetDefaulTracker("TRACKER_ID");
@@ -34,10 +40,10 @@ public class Main : MonoBehaviour
 
         //var callback = new AndroidPluginCallback();
         //callback.OnSuccess += (count) => { 
-        
+
         //};
         //callback.OnError += (exception) => { 
-        
+
         //};
 
         //chabok.AddTag("Test", callback);
@@ -47,12 +53,22 @@ public class Main : MonoBehaviour
 
 
         //chabok.Track("LIKE", null);
-
     }
+
+    //private void Chabok_ConnectionChanged(string result)
+    //{
+    //    Log("Chabok connection status is " + result);
+    //}
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void Log(object text)
+    {
+        txt.text += "\n";
+        txt.text += text;
     }
 }
